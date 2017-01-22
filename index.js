36b8836bb9df44c4c7dc689216322b6bd916bfcb
Tim Officer <timothyofficer@gmail.com>
2017-01-22 18:46:09 -0500

    add !important to rules

--- index.js
+++ index.js
@@ -5,5 +5,17 @@ module.exports = function (source) {
 
   const ast = css.parse(source);
 
+  ast.stylesheet.rules.forEeach(rule => {
+    if (rule.type === 'rule') {
+      console.log('rule before', rule.value);
+
+      if (!rule.value.endsWith('!important')) {
+        rule.value += ' !important';
+      }
+
+      console.log('rule after', rule.value);
+    }
+  });
+
   return css.stringify(ast);
 };
