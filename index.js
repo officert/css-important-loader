0e203c4c2e8cb0483201504194e1535d1f5511b7
Tim Officer <timothyofficer@gmail.com>
2017-01-22 20:03:13 -0500

    cleanup

--- index.js
+++ index.js
@@ -6,18 +6,12 @@ module.exports = function (source) {
   const ast = css.parse(source);
 
   ast.stylesheet.rules.forEach(rule => {
-    console.log('rule', rule);
-
     if (rule.type === 'rule' && rule.declarations && rule.declarations.length) {
       rule.declarations.forEach(declaration => {
         if (declaration.type === 'declaration') {
-          console.log('processing', declaration.type);
-
           if (!declaration.value.endsWith('!important')) {
             declaration.value += ' !important';
           }
-        } else {
-          console.log('skipping', declaration.type);
         }
       });
     }
